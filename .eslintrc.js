217ea0e7b9e6611c5f18a778fab6dd07d003dcd2
mikeyfarina <mikeyfarina7@gmail.com>
2021-01-25 03:52:56 -0500

    removed clg warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
     'arrow-spacing': ['error', { before: true, after: true }],
-    'no-console': ['warn'],
+    //'no-console': ['warn'],
     'react/prop-types': 0,
   },
   settings: {
