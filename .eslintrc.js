6685837c1748ca0c4f8b8569936fb4592345a634
mikeyfarina <mikeyfarina7@gmail.com>
2021-01-25 03:22:06 -0500

    rm eslint cypress env variable

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
     browser: true,
     es6: true,
     'jest/globals': true,
-    'cypress/globals': true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
