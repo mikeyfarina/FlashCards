ac0e7e0a976ea5d2f5392f77185515bdfb7c8036
mikeyfarina <mikeyfarina7@gmail.com>
2021-01-25 17:49:38 -0500

    change app router

--- app.js
+++ app.js
@@ -49,14 +49,9 @@ if (process.env.NODE_ENV === 'test') {
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
-if (
-  process.env.NODE_ENV === 'production' ||
-  process.env.NODE_ENV === 'staging'
-) {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-  });
-}
+app.use(express.static('client/build'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 module.exports = app;
