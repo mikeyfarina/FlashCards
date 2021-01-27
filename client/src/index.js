1e0fcbf95e4f8347291345f8bd60aa01be59b938
mikeyfarina <mikeyfarina7@gmail.com>
2021-01-27 04:02:26 -0500

    add react router

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,17 @@ import './fontawesome';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import App from './App.js';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
+      <Switch>
+        <Route path={'/'}
+      </Switch>
+    </Router>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
