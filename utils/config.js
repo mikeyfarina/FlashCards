a5579307fdc98de54e773db592115b1cf4cda4aa
mikeyfarina <mikeyfarina7@gmail.com>
2021-03-23 22:43:26 -0400

    fixed config

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,6 @@
 require('dotenv').config();
 
-<<<<<<< HEAD
 const PORT = 3001;
-=======
-const PORT = process.env.PORT || 3001;
->>>>>>> d6bf88125bec33c50e981e5a4de7544b403c7ff1
 let { MONGODB_URI } = process.env;
 
 if (process.env.NODE_ENV === 'test') {
