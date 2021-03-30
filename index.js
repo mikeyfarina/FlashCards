835c5e795ff7df9539712cb2bbd608e68d63bfc4
mikeyfarina <mikeyfarina7@gmail.com>
2021-03-29 21:32:31 -0400

    undo revert

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const config = require('./utils/config');
 
 const server = http.createServer(app);
 
-server.listen(config.PORT, () => {
+server.listen(process.env.PORT || config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
 });
